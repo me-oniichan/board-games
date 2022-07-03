0e7b064fbfa4bc668fb552d4f8081c1106708d00
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-07-03 14:24:17 +0530

    Added more styling

--- src/Sudoku/Board.jsx
+++ src/Sudoku/Board.jsx
@@ -6,16 +6,18 @@ export default function Board() {
   const [grid, setGrid] = useContext(sudokuContext);
 
   function setval(e, i, j) {
-    setGrid(grid.map((val1, ind1) => (
-      val1.map((val2, ind2) => {
-        if (ind1 === i && ind2 === j) {
-
-          if(!isNaN(e)) val2 = { ...val2, value: e , mode : "black"}
-          else if(e === "Backspace") val2 = { ...val2, value: 0 , mode : "#3838f9ed"}
-        }
-        return val2;
-      })
-    )))
+    setGrid(
+      grid.map((val1, ind1) =>
+        val1.map((val2, ind2) => {
+          if (ind1 === i && ind2 === j) {
+            if (!isNaN(e)) val2 = { ...val2, value: e, mode: "black" };
+            else if (e === "Backspace")
+              val2 = { ...val2, value: 0, mode: "#3838f9ed" };
+          }
+          return val2;
+        })
+      )
+    );
   }
 
   return (
@@ -23,11 +25,23 @@ export default function Board() {
       {grid.map((val1, ind1) => (
         <Row key={ind1}>
           {val1.map((val2, ind2) => (
-            <Col className={val2.class} key={ind1.toString() + ind2.toString()} tabIndex="0" onKeyDown={(e) => { setval(e.key, ind1, ind2) }}>
-              <span style={{
-                margin: "auto",
-                color: val2.mode
-              }}>{val2.value ? val2.value : ""}</span>
+            <Col
+              className={val2.class}
+              key={ind1.toString() + ind2.toString()}
+              tabIndex="0"
+              onKeyDown={(e) => {
+                setval(e.key, ind1, ind2);
+              }}
+            >
+              <input className="dummy" type="number" value={''}/>
+              <span
+                style={{
+                  margin: "auto",
+                  color: val2.mode,
+                }}
+              >
+                {val2.value ? val2.value : ""}
+              </span>
             </Col>
           ))}
         </Row>
--- src/Sudoku/Sudoku.js
+++ src/Sudoku/Sudoku.js
@@ -3,17 +3,19 @@ import Board from "./Board";
 import sudokuContext from "./sudokuContext";
 
 export default function Sudoku() {
-  const grid = {};
+    const grid = {};
 
-  const board = useState(new Array(9).fill(0).map(() => new Array(9).fill({
-    value : 0,
-    class : "col",
-    mode : "#3838f9ed"
-  })));
+    const board = useState(new Array(9).fill(0).map(() => new Array(9).fill({
+        value: 0,
+        class: "col",
+        mode: "#3838f9ed"
+    })));
 
-  return(
-      <sudokuContext.Provider value = {board}>
-          <Board></Board>
-      </sudokuContext.Provider>
-  );
+    return (
+        <sudokuContext.Provider value={board}>
+            <div className="wrapper" >
+                <Board></Board>
+            </div>
+        </sudokuContext.Provider>
+    );
 }
