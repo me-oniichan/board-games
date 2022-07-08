eb53540dc557eaeca3704352fbc3309735f5916b
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-07-08 18:19:16 +0530

    styles solve button

--- src/Sudoku/Solve.jsx
+++ src/Sudoku/Solve.jsx
@@ -1,20 +1,28 @@
-import { useContext } from "react";
-import { Button } from "reactstrap";
+import {useContext, useState} from "react";
+import { Button, Tooltip } from "reactstrap";
 import sudokuContext from "./sudokuContext";
 import { solveGrid } from "./utils";
+import dupContext from "./dupContext";
 
 export default function Solve() {
     const [grid, setGrid] = useContext(sudokuContext);
+    const duplicate = useContext(dupContext)[0];
+    const [ttip, setTtip] = useState(duplicate.length? true : false)
     return (
-        <Button
-            color="success"
-            onClick={() => {
-                const val = solveGrid(grid, 0, 0);
-                if (val)
-                    setGrid([...val]);
-            }}
-        >
-            Solve
-        </Button>
+        <div>
+            <Button
+                id="solve"
+                color={duplicate.length ? "danger" : "success"}
+                onClick={duplicate.length ? ()=>{}: () => {
+                    const val = solveGrid(grid, 0, 0);
+                    if (val) setGrid([...val]);
+                }}
+            >
+                Solve
+            </Button>
+            {duplicate.length? (<Tooltip flip target="solve" isOpen={ttip}   toggle={()=>{setTtip(!ttip)}} placement="top">
+                Invalid Puzzel
+            </Tooltip>) : ('')}
+        </div>
     );
 }
