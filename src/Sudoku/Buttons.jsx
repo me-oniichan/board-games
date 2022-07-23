359f7d6103b9295743790a18a79bebe009e02d9b
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-07-23 18:06:32 +0530

    added reset button

--- src/Sudoku/Buttons.jsx
+++ src/Sudoku/Buttons.jsx
@@ -1,10 +1,12 @@
+import Reset from "./Reset";
 import Solve from "./Solve";
 
 
 export default function Buttons(){
     return(
         <div style={{margin: "5px", fontSize:"1rem"}}>
-            <Solve></Solve>
+            <Solve/>
+            <Reset/>
         </div>
     )
 }
---
+++ src/Sudoku/Reset.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { Button } from "reactstrap";
+import sudokuContext from "./sudokuContext";
+
+export default function Reset(){
+    const [grid, setGrid] = useContext(sudokuContext)
+
+    const reset = ()=>{
+        setGrid(grid.map((val1)=>(
+            val1.map((val2)=>{
+                return val2.mode === "#5b50b1"? {...val2, value: 0} : val2;
+            })
+        )))
+    }
+
+    return(
+        <Button id="reset" color="warning" onClick={reset}>Reset</Button>
+    )
+}
