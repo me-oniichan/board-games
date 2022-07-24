9c3342c20830078d830ad0c1a0f48f204ff4e0cd
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-07-24 09:01:34 +0530

    added animation

--- src/Sudoku/Buttons/Animate.jsx
+++ src/Sudoku/Buttons/Animate.jsx
@@ -11,40 +11,35 @@ export default function Animate() {
     const duplicate = useContext(dupContext)[0];
     const [ttip, setTtip] = useState(duplicate.length ? true : false);
 
+    let v = animate;
+
     function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+        return new Promise((resolve) => setTimeout(resolve, ms));
     }
 
-    const toogleState = async (row, col, value) => {
-        setAnimate(
-            animate.map((val, ind1) =>
+    const toggleState = async (row, col, value) => {
+        v[row][col] = true;
+        setAnimate(v);
+
+        setGrid(
+            grid.map((val, ind1) =>
                 val.map((val2, ind2) => {
-                    return (ind1 === row && ind2 === col)? true : val2;
+                    return ind1 === row && ind2 === col
+                        ? { ...val2, value: value }
+                        : val2;
                 })
             )
         );
 
-        setGrid(grid.map((val, ind1)=>(
-            val.map((val2, ind2)=>{
-                return (ind1 === row && ind2 === col)? {...val2, value : value} : val2;
-            })
-        )))
-
-        await sleep(100)
+        await sleep(100);
     };
 
-    const togleNormal = (row, col)=>{
-        setAnimate(
-            animate.map((val, ind1) =>
-                val.map((val2, ind2) => {
-                    return (ind1 === row && ind2 === col)? false : val2;
-                })
-            )
-        );
-    }
+    const toggleNormal = (row, col) => {
+        v[row][col] = false;
+        setAnimate(v);
+    };
 
-
-    const animation = () => {
+    const animation = async () => {
         const solve = async (grid, row, col) => {
             if (row > 8 && col >= 8) return grid;
             else if (row > 8) {
@@ -58,17 +53,17 @@ export default function Animate() {
             for (let i = 1; i <= 9; i++) {
                 if (isvalid(grid, row, col, i)) {
                     grid[row][col].value = i;
-                    await toogleState(row, col, i)
-                    if (await solve(grid, row + 1, col)){
+                    await toggleState(row, col, i);
+                    if (await solve(grid, row + 1, col)) {
                         return grid;
-                    }
-                    else grid[row][col].value = 0;
+                    } else grid[row][col].value = 0;
                 }
             }
-            togleNormal(row, col);
+            toggleNormal(row, col);
             return false;
         };
-        solve(grid, 0, 0);
+        await solve(grid, 0, 0);
+        setAnimate(animate.map((v) => v.map(() => false)));
     };
 
     return (
@@ -76,7 +71,13 @@ export default function Animate() {
             <Button
                 id="animate"
                 color={duplicate.length ? "danger" : "primary"}
-                onClick = {animation}
+                onClick={
+                    duplicate.length
+                        ? () => {}
+                        : () => {
+                              animation();
+                          }
+                }
             >
                 Animate
             </Button>
