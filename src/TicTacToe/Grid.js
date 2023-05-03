7d35b95adeda6565d94827b8e7a3c778db6e9671
Onii Chan <66708802+me-oniichan@users.noreply.github.com>
2023-05-03 05:36:36 +0000

    changed tic tac toe color

--- src/TicTacToe/Grid.js
+++ src/TicTacToe/Grid.js
@@ -53,7 +53,7 @@ const Grid = () => {
       <div className="msg" id="msg">
         {msg}
       </div>
-      <div className="text-bg-warning grid">
+      <div className="bg-card grid">
         {data.map((element, index) => (
           <div className="box" key={index} onClick={() => playTurn(index)}>
             <Icon icon={element} />
